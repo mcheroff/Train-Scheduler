ea774066849a2b7a08fa472645dfad7c4fdd7882
Michael Cheroff <michael.cheroff@gmail.com>
2016-11-27 12:57:43 -0700

    configured firebase link

---
+++ traintime-logic.js
@@ -0,0 +1,7 @@
+var config = {
+	apiKey: "",
+	authDomain: "howtotrainyoursched.firebaseapp.com",
+	databaseURL: "https://howtotrainyoursched.firebaseio.com/",
+	storageBucket: "howtotrainyoursched.appspot.com"
+};
+firebase.initializeApp(config);
